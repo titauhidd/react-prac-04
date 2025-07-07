7ee8802d465115e8fd33f3c7fc3dd71c85121163
titauhidd <tauhidultonmoy1234@gmail.com>
2025-07-07 22:54:31 +0600

    used usestate

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import Linkss from "../Linkss/Linkss";
+import { AiOutlineBars, AiTwotoneCloseSquare } from "react-icons/ai";
 
 const Navbar = () => {
   const routes = [
     { id: 1, path: "/", name: "Home" },
-    { id: 1, path: "/about", name: "About" },
-    { id: 1, path: "/services", name: "Services" },
-    { id: 1, path: "/contact", name: "Contact" },
-    { id: 1, path: "*", name: "Not Found" },
+    { id: 2, path: "/about", name: "About" },
+    { id: 3, path: "/services", name: "Services" },
+    { id: 4, path: "/contact", name: "Contact" },
+    { id: 5, path: "*", name: "Not Found" },
   ];
+
+  const [open, setOpen] = useState(false);
 
   return (
     <nav className="bg-amber-400">
       {/* <h2>My nav</h2> */}
+      <div
+        onClick={() => {
+          setOpen(!open);
+        }}
+        className="text-3xl"
+      >
+        {open === true ? (
+          <AiTwotoneCloseSquare></AiTwotoneCloseSquare>
+        ) : (
+          <AiOutlineBars></AiOutlineBars>
+        )}
+      </div>
+
       <ul className="flex gap-8 text-2xl">
         {routes.map((routeli) => {
           return <Linkss key={routeli.id} routeli={routeli}></Linkss>;
